ab93d5dfbb4d2075ff782cbb83546113b3849a41
Кирилл Прокофьев <prokyhouse@mail.ru>
2022-11-18 19:57:37 +0700

    Создал Maven проект для коннектора

---
+++ connector/src/main/java/ru/nsu/connector/App.java
@@ -0,0 +1,10 @@
+package ru.nsu.connector;
+/**
+ * Created by Kirill Prokofiev on 18.11.2022.
+ * MIT License.
+ */
+
+public class App 
+{
+    public static void main( String[] args ) { }
+}
---
+++ connector/src/test/java/ru/nsu/connector/AppTest.java
@@ -0,0 +1,17 @@
+package ru.nsu.connector;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for Connector.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
