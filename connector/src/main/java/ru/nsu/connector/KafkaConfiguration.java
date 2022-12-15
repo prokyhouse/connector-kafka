ae18252db3ad5451d2ea9c78750ec40a34ee11d1
Кирилл Прокофьев <prokyhouse@mail.ru>
2022-12-15 23:27:47 +0700

    Добавлены базовые имплементации коннектора

--- connector/src/main/java/ru/nsu/connector/KafkaConfiguration.java
+++ connector/src/main/java/ru/nsu/connector/KafkaConfiguration.java
@@ -1,8 +1,21 @@
 package ru.nsu.connector;
 
-public class KafkaConfiguration {
+import org.identityconnectors.framework.spi.AbstractConfiguration;
+import org.identityconnectors.framework.spi.StatefulConfiguration;
+
+public class KafkaConfiguration extends AbstractConfiguration implements StatefulConfiguration {
 
     protected ConnectorUseCase connectorUseCase;
+
+    @Override
+    public void validate() {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
 
     protected enum ConnectorUseCase {
         CONSUMER,
--- connector/src/main/java/ru/nsu/connector/KafkaConnector.java
+++ connector/src/main/java/ru/nsu/connector/KafkaConnector.java
@@ -1,21 +1,85 @@
 package ru.nsu.connector;
 
+import java.util.*;
+
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.spi.Configuration;
+import org.identityconnectors.framework.spi.Connector;
+import org.identityconnectors.framework.spi.ConnectorClass;
+import org.identityconnectors.framework.spi.operations.*;
+
+
 /**
  * Created by Kirill Prokofiev on 18.11.2022.
  * MIT License.
  */
 
-public class KafkaConnector {
+@ConnectorClass(displayNameKey = "connector.kafka.display", configurationClass = KafkaConfiguration.class)
+public class KafkaConnector implements TestOp, SchemaOp, Connector, SyncOp, CreateOp, UpdateDeltaOp, DeleteOp {
 
     protected KafkaConfiguration configuration;
 
     public static void main( String[] args ) { }
 
+    private static final Log LOGGER = Log.getLog(KafkaConnector.class);
+
     public void init(KafkaConfiguration configuration) {
+
         if (configuration.isConsumer()) {
             System.out.println("[STATE] Connector is CONSUMER");
         } else if (configuration.isProducer()) {
             System.out.println("[STATE] Connector is PRODUCER");
         }
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return null;
+    }
+
+    @Override
+    public void init(Configuration configuration) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public Uid create(ObjectClass objectClass, Set<Attribute> set, OperationOptions operationOptions) {
+        return null;
+    }
+
+    @Override
+    public void delete(ObjectClass objectClass, Uid uid, OperationOptions operationOptions) {
+
+    }
+
+    @Override
+    public Schema schema() {
+        return null;
+    }
+
+    @Override
+    public void sync(ObjectClass objectClass, SyncToken syncToken, SyncResultsHandler syncResultsHandler, OperationOptions operationOptions) {
+
+    }
+
+    @Override
+    public SyncToken getLatestSyncToken(ObjectClass objectClass) {
+        return null;
+    }
+
+    @Override
+    public void test() {
+
+    }
+
+    @Override
+    public Set<AttributeDelta> updateDelta(ObjectClass objectClass, Uid uid, Set<AttributeDelta> set, OperationOptions operationOptions) {
+        return null;
     }
 }
