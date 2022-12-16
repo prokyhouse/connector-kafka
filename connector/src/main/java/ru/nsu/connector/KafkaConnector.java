df2f7c395a34de9b4df124a39989e2246debdc81
Кирилл Прокофьев <prokyhouse@mail.ru>
2022-12-16 09:52:34 +0700

    Добавлено создание продьюсера

--- connector/src/main/java/ru/nsu/connector/KafkaConnector.java
+++ connector/src/main/java/ru/nsu/connector/KafkaConnector.java
@@ -1,7 +1,19 @@
 package ru.nsu.connector;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
+import com.hortonworks.registries.schemaregistry.serdes.avro.AbstractAvroSnapshotDeserializer;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.record.Record;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.common.objects.*;
 import org.identityconnectors.framework.spi.Configuration;
@@ -19,19 +31,8 @@ import org.identityconnectors.framework.spi.operations.*;
 public class KafkaConnector implements TestOp, SchemaOp, Connector, SyncOp, CreateOp, UpdateDeltaOp, DeleteOp {
 
     protected KafkaConfiguration configuration;
-
+    private KafkaProducer<String, Record> producer;
     public static void main( String[] args ) { }
-
-    private static final Log LOGGER = Log.getLog(KafkaConnector.class);
-
-    public void init(KafkaConfiguration configuration) {
-
-        if (configuration.isConsumer()) {
-            System.out.println("[STATE] Connector is CONSUMER");
-        } else if (configuration.isProducer()) {
-            System.out.println("[STATE] Connector is PRODUCER");
-        }
-    }
 
     @Override
     public Configuration getConfiguration() {
@@ -40,7 +41,60 @@ public class KafkaConnector implements TestOp, SchemaOp, Connector, SyncOp, Crea
 
     @Override
     public void init(Configuration configuration) {
+        if (this.configuration.isConsumer()) {
+            System.out.println("[STATE] Connector is CONSUMER");
+        } else if (this.configuration.isProducer()) {
+            System.out.println("[STATE] Connector is PRODUCER");
+        }
 
+        KafkaConfiguration kafkaConfig = (KafkaConfiguration) configuration;
+        this.configuration = kafkaConfig;
+        this.configuration.validate();
+
+        if (((KafkaConfiguration)configuration).isProducer()) {
+            this.producer = this.initProducer();
+        }
+    }
+
+    private KafkaProducer<String, Record> initProducer() {
+        Properties properties = new Properties();
+        String pathToMorePropertiesForProducer = configuration.getPathToMorePropertiesForProducer();
+
+        if(pathToMorePropertiesForProducer != null) {
+            try (InputStream input = new FileInputStream(pathToMorePropertiesForProducer)) {
+                properties.load(input);
+            } catch (IOException ignored) { }
+        }
+
+        addCommonPropertiesForConsumerAndProducer(configuration, properties);
+
+        if(properties.contains(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG)) {
+            properties.remove(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG);
+        }
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        if(properties.contains(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG)) {
+            properties.remove(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG);
+        }
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, MidpointKafkaAvroSerializer.class);
+
+        return new KafkaProducer(properties);
+    }
+
+    private static void addCommonPropertiesForConsumerAndProducer(KafkaConfiguration configuration, Properties properties) {
+        if(properties.contains(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG)) {
+            properties.remove(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG);
+        }
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, configuration.getBootstrapServers());
+
+//        if(properties.contains(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG)) {
+//            if(configuration.getKafkaSecurityProtocol() != null) {
+//                properties.remove(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG);
+//                properties.put(properties, CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, configuration.getKafkaSecurityProtocol());
+//            }
+//        } else {
+//            properties.put(properties, CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, configuration.getKafkaSecurityProtocol());
+//        }
     }
 
     @Override
