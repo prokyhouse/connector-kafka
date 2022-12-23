a8f84ab53666551db880b47f5ed695e0e2a52bd2
Кирилл Прокофьев <prokyhouse@mail.ru>
2022-12-23 11:13:15 +0700

    Исправление коллбэка и рефакторинг

--- connector/src/main/java/ru/nsu/connector/PublishCallback.java
+++ connector/src/main/java/ru/nsu/connector/PublishCallback.java
@@ -2,20 +2,10 @@ package ru.nsu.connector;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.RecordMetadata;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 
 public class PublishCallback implements Callback {
 
-//    private static final Logger LOG = LoggerFactory.getLogger(PublishCallback.class);
-
     @Override
-    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-        if (e == null) {
-//            LOG.debug("Record has been published successfully.");
-        } else {
-//            LOG.warn("Failed to publish the record.", e);
-        }
-    }
+    public void onCompletion(RecordMetadata recordMetadata, Exception e) { }
 
 }
